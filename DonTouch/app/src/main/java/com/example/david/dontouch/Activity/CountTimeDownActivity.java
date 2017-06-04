4fab3478aad968a75b3f7cdf3cdce0ef910c155a
louis0526 <14301030@bjtu.edu.cn>
2017-06-04 12:43:37 +0800

    关屏挑战更新

--- DonTouch/app/src/main/java/com/example/david/dontouch/Activity/CountTimeDownActivity.java
+++ DonTouch/app/src/main/java/com/example/david/dontouch/Activity/CountTimeDownActivity.java
@@ -1,67 +1,43 @@
 package com.example.david.dontouch.Activity;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.david.dontouch.R;
 import com.example.david.dontouch.View.CircleSeekBar;
 
 public class CountTimeDownActivity extends AppCompatActivity {
-    private Activity activity;
-    private int hour;
-    private int min;
-    private int hour_now;
-    private int min_now;
+    private int time;
     private final boolean DEBUG = true;
     private final String TAG = "MainActivity";
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //activity = this;
-        //CommonUtil.setNoTitleBar(activity);
-        //CommonUtil.setFullScreen(activity);
         setContentView(R.layout.activity_count_time_down);
 
-        CircleSeekBar circleSeekBar = (CircleSeekBar) findViewById(R.id.circle_seekbar);
+        final CircleSeekBar circleSeekBar = (CircleSeekBar) findViewById(R.id.circle_seekbar);
         circleSeekBar.setProgress(0);
         circleSeekBar.setOnSeekBarChangeListener(new CircleSeekBarOnChangeListener());
 
-        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window window = getWindow();
-            // Translucent status bar
-            window.setFlags(
-                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
-                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }*/
-        //透明状态栏
-        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        //透明导航栏
-        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-
         init();
-        /*button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(CountTimeDownActivity.this, CountDownActivity.class);
-                Log.i("timeaaaaa", "" + (min-min_now));
-                intent.putExtra("time", (hour-hour_now)*3600 + (min-min_now)*60000);
+                time = circleSeekBar.getProgress();
+                Log.i(TAG, Integer.toString(circleSeekBar.getProgress()));
+                intent.putExtra("time", time * 60000);
                 startActivity(intent);
                 finish();
             }
-        });*/
-
-        /*timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-                hour = hourOfDay;
-                min = minute;
-
-            }
-        });*/
+        });
     }
 
     private class CircleSeekBarOnChangeListener implements CircleSeekBar.OnSeekBarChangeListener {
@@ -85,11 +61,7 @@ public class CountTimeDownActivity extends AppCompatActivity {
 
     public void init() {
         long time = System.currentTimeMillis();
-        //button = (Button) findViewById(R.id.count_down_btn);
-        //timePicker = (TimePicker) findViewById(R.id.time_picker);
-        //calendar = Calendar.getInstance();
-        //calendar.setTimeInMillis(time);
-        //hour_now = calendar.get(Calendar.HOUR);
-        //min_now = calendar.get(Calendar.MINUTE);
+        button = (Button) findViewById(R.id.bAddOne);
+        time = 120;
     }
 }
